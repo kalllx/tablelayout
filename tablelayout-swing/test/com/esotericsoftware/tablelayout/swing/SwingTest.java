90198713a2761b0356fe70c17494f589b4831395
kalllx <kalllx@yahoo.cn>
2014-03-17 18:04:15 +0800

    Signed-off-by: kalllx <kalllx@yahoo.cn>

--- tablelayout-swing/test/com/esotericsoftware/tablelayout/swing/SwingTest.java
+++ tablelayout-swing/test/com/esotericsoftware/tablelayout/swing/SwingTest.java
@@ -1,14 +1,15 @@
 
 package com.esotericsoftware.tablelayout.swing;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.awt.GraphicsConfiguration;
-import java.awt.HeadlessException;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
 
 public class SwingTest extends JFrame {
 
@@ -16,33 +17,75 @@ public class SwingTest extends JFrame {
 		setTitle("SwingTest");
 		setSize(640, 480);
 		setLocationRelativeTo(null);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 
-		JButton button1 = new JButton("One");
-		JButton button2 = new JButton("Two");
-		JButton button3 = new JButton("Three");
-		JButton button4 = new JButton("Four");
-		JButton button5 = new JButton("Five");
-		JTextField text1 = new JTextField("One");
-		JTextField text2 = new JTextField("Two");
-		JTextField text3 = new JTextField("Three");
-
+		 
+		Table table1 = new Table();
+		getContentPane().add(table1);
+		table1.top().left();
+		
+		
 		Table table = new Table();
-		getContentPane().add(table);
-
-		table.addCell(button1);
-		table.addCell(button2);
+		table.columnDefaults(0).width(50).right();
+		table.columnDefaults(1).expandX().left();
+		table.columnDefaults(2).expandX().left();
+		table.columnDefaults(3).expandX().left();
+		table.columnDefaults(4).expandX().left();
+		table.addCell(new JLabel("xxx"));
+		table.addCell(new JLabel("111"));
+		table.addCell(new JLabel("3324.3"));
+		table.addCell(new JLabel("232.44"));
+		
 		table.row();
-		table.addCell(button3).colspan(2);
-		table.row();
-		table.addCell(text1).colspan(2).fill();
-		table.row();
-		table.addCell(text2).right().width(150);
-		table.addCell(text3).width(250);
-		table.row();
-		table.addCell(button4).expand().colspan(2);
-		table.debug();
+		table.addCell(new JLabel("xxx"));
+		table.addCell(new JLabel("111"));
+		table.addCell(new JLabel("3324.3"));
+		table.addCell(new JLabel("232.44"));
+		
+		
+		
+		
+		Table table2  = new Table();
+		table2.addCell(new JLabel("232.44dd"));
+		table2.row();
+		table2.addCell(new JLabel("232.44dd"));
+		
+		table1.addCell(table).expandX().prefWidth(10000).top();
+		table1.addCell(table2).width(200).top();
+		table1.row();
+		Table dd = new Table();
+		dd.defaults().width(100).pad(10);
+		dd.addCell("dfdf").expand();
+		dd.addCell("dfdf");
+		dd.addCell("dfdf");
+		dd.addCell("dfdf");
+		table1.addCell(dd).colspan(2).prefWidth(5515);
+		table1.row();
+		
+		
+		Table caozuoqu = new Table();
+		JPanel p = new JPanel();
+		p.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED ));
+		p.add(new JButton("buy"));
+		p.add(new JButton("buy"));
+		p.add(new JButton("buy"));
+		p.add(new JButton("buy"));
+		p.setPreferredSize(new Dimension(200, 200));
+		caozuoqu.addCell(p);
+		caozuoqu.addCell(new JButton("fdfd")).expand();
+		
+		table1.addCell(caozuoqu).colspan(2).prefWidth(5515);
+		
+		table1.row().height(200);
+		Table tt = new Table();
+		tt.addCell("33");
+		tt.addCell(new JButton("fdfd")).expand();
+		
+		tt.addCell("33");
+		
+		table1.addCell(tt).colspan(2).prefWidth(5515);
+		table1.debug();
 	}
 
 	public static void main (String[] args) throws Exception {
